53ed5655a6ca5e840ea6cebf21b0c10eb429472d
anwerkamalrizvi <sanwerkamal@gmail.com>
2024-10-29 10:34:28 +0500

    Add files via upload

---
+++ reverse.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() { // start main
+    char name[100];
+    int length;
+    printf("Enter your name: ");
+    fgets(name, sizeof(name), stdin);
+
+    length = strlen(name);
+    if (name[length - 1] == '\n') {
+        name[length - 1] = '\0';
+    }
+
+    printf("Reversed name: ");
+    for (int i = length - 2; i >= 0; i--) {
+        putchar(name[i]);
+    }
+    printf("\n");
+
+    return 0;
+} // end main
