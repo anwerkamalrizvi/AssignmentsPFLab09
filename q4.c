23bddc9c73f4c0aa9617ee0db71b91a89a2bf26c
anwerkamalrizvi <sanwerkamal@gmail.com>
2024-11-23 14:05:22 +0500

    Update q4.c

--- q4.c
+++ q4.c
@@ -1,49 +1,38 @@
 /*Write a program that initializes a 2D character array with a list of words. Then, take a
 word as input from the user and check if it exists in the array. Display "Found" if it’s there,
 otherwise display "Not Found".*/
+#include <windows.h>
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
-#include <windows.h>
-#define string_size 16
-#define letters 100
 
-void ifThereIsWord(char str[string_size][letters]){
-    char word[4];
+#define STRING_SIZE 16 
+#define LETTERS 100    
+
+void ifThereIsWord(char str[STRING_SIZE][LETTERS]) {
+    char word[LETTERS];
+
     printf("\nEnter the word to search: ");
-    scanf("%s",word); char count[100];
-    for(int i=0;i<4;i++){
-        word [i]= toupper(word[i]);
-    }//end for loop
-    int len = strlen(word); int ctr=0; int find=0;
-    char store[string_size][letters];
-    switch(len){
-        case 2:
-        for(int i=0;i<string_size;i++){
-                int strl = strlen(str[i]);
-                if(strl==2){
-                    printf("\nFound at %d ",i+1);
-                    for(int j=0;j<strl;j++){
-                        store[ctr][j] = str[i][j];
-                        printf("\nAt i= %d, I am store[%d][%d]: %c ",i,ctr,j,store[ctr][j]);
-                    }//end for loop
-                    ctr+=1; 
-                }
-           // }//end for loop
-        }//end for loop
-            int i=0; int check =0;
-            for(int k=0;k<ctr;k++){
-                if(store[check][i] == word[i] && store[check][i+1] == word[i+1]){
-                    printf("\nFound ");
-                }else check++;
-                
-            }//end for loop
-        break;
+    scanf("%s", word);
+
+
+    for (int i=0; i<strlen(word); i++) {
+        word[i] = toupper(word[i]);
     }
-}//end function
+
+    for (int i=0; i<STRING_SIZE; i++) {
+        if (strcmp(str[i], word) == 0) {
+            printf("\nFound");
+            return;
+        }
+    }
+    printf("\nNot Found\n"); 
+}
+
 int main() {
-    char str[string_size][letters] = {"LAB","KEY","FIN","NOW","SOON","ASAP","READ","ROLE",
-                                     "PIE","BYE","HEYE","LIE", "GO","DIE","HELL","IN"};        
+    char str[STRING_SIZE][LETTERS] = {
+        "LAB", "KEY", "FIN", "NOW", "SOON", "ASAP", "READ", "ROLE",
+        "PIE", "BYE", "HEY", "LIE", "GO", "DIE", "HELL", "IN"};
     system("cls");
     ifThereIsWord(str);
     return 0;
